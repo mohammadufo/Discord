9d7c2fc7cf4150311003ef9a25662d261e99b3d1
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-06-16 00:44:48 +0330

    frontend - add react clerk

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,18 @@ import App from './App.tsx'
 import './index.css'
 import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
+import { SignedOut, RedirectToSignIn, SignedIn } from '@clerk/clerk-react'
+
+const protectedRoute = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <SignedIn>{children}</SignedIn>
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
