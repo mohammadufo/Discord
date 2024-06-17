e044c179233c34d6fd74fd6d8377c6509ed35cb1
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-06-17 13:17:52 +0330

    frontend - add zustand and general store

---
+++ frontend/src/stores/generalStore.ts
@@ -0,0 +1,33 @@
+import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
+
+export type Modal =
+  | 'CreateServer'
+  | 'InvitePeople'
+  | 'UpdateServer'
+  | 'CreateChannel'
+  | 'ManageMembers'
+  | 'DeleteChannel'
+  | 'UpdateChannel'
+  | 'DeleteServer'
+  | 'ServerJoin'
+  | 'UpdateMessage'
+  | 'LeaveServer'
+
+interface GeneralStore {
+  activeModal: Modal | null
+  setActiveModal: (modal: Modal | null) => void
+}
+
+export const useGeneralStore = create<GeneralStore>()(
+  persist(
+    (set) => ({
+      activeModal: null,
+
+      setActiveModal: (modal: Modal | null) => set({ activeModal: modal }),
+    }),
+    {
+      name: 'general-store',
+    }
+  )
+)
