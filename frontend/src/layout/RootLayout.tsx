46045316addaf07fdd2ce33708cebb73d32bc6fc
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-06-17 11:56:51 +0330

    front end - configure routes and layouts

---
+++ frontend/src/layout/RootLayout.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom'
+
+const RootLayout = () => {
+  return (
+    <div>
+      Alaa💕
+      <Outlet />
+    </div>
+  )
+}
+
+export default RootLayout
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,9 +4,17 @@ import App from './App.tsx'
 import './index.css'
 import { MantineProvider } from '@mantine/core'
 import '@mantine/core/styles.css'
-import { SignedOut, RedirectToSignIn, SignedIn } from '@clerk/clerk-react'
+import {
+  SignedOut,
+  RedirectToSignIn,
+  SignedIn,
+  ClerkProvider,
+} from '@clerk/clerk-react'
+import { useNavigate, Routes, Route, BrowserRouter } from 'react-router-dom'
+import RootLayout from './layout/RootLayout.tsx'
+import HomePage from './pages/HomePage.tsx'
 
-const protectedRoute = ({ children }: { children: React.ReactNode }) => {
+const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <SignedIn>{children}</SignedIn>
@@ -16,11 +24,38 @@ const protectedRoute = ({ children }: { children: React.ReactNode }) => {
     </>
   )
 }
+const RouterComponent = () => {
+  const navigate = useNavigate()
+
+  return (
+    <ClerkProvider
+      publishableKey={import.meta.env.VITE_CLERK_PUBLISHABLE_KEY}
+      navigate={(to) => navigate(to)}
+    >
+      <Routes>
+        <Route path="" element={<RootLayout />}>
+          <Route
+            index={true}
+            element={
+              <ProtectedRoute>
+                <HomePage />
+              </ProtectedRoute>
+            }
+          />
+        </Route>
+      </Routes>
+    </ClerkProvider>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <MantineProvider>
-      <App />
+      <BrowserRouter>
+        <RouterComponent />
+      </BrowserRouter>
     </MantineProvider>
   </React.StrictMode>
 )
+
+export default RouterComponent
