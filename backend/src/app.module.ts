dc2c024d4dbbc2fd656a2fb8513a6604267690b7
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-06-17 22:13:22 +0330

    backend - configure graphql

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
       serveRoot: '/',
+    }),
+
+    GraphQLModule.forRootAsync({
+      imports: [],
+      inject: [],
+      driver: ApolloDriver,
+      useFactory: async () => {
+        return {
+          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+          sortSchema: true,
+          subscriptions: {},
+        };
+      },
     }),
   ],
   controllers: [AppController],
