4edd18212f46d7eb2cb37ce9ac935141f0499ce1
Macbook <shadow.m.s.313@gmail.com>
2024-06-18 00:06:01 +0330

    test update

--- backend/src/profile/profile.service.ts
+++ backend/src/profile/profile.service.ts
@@ -7,6 +7,7 @@ export class ProfileService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async createProfile(createProfileDto: CreateProfileDto) {
+    console.log('test!');
     const profile = await this.prismaService.profile.findUnique({
       where: {
         email: createProfileDto.email,
