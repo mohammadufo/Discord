070e91a44eaec3f25e41fda98bdeabefef77a3c1
Macbook <macbook@Macbooks-MacBook-Pro.local>
2024-06-17 22:16:03 +0330

    backend - configure cors policy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+    allowedHeaders: [
+      'Accept',
+      'Authorization',
+      'Content-Type',
+      'X-Requested-With',
+      'apollo-require-preflight',
+    ],
+  });
+
   await app.listen(3000);
 }
 bootstrap();
