f9342a4fdc5a189b639ac92b396c3909b8e96015
Macbook <shadow.m.s.313@gmail.com>
2024-06-18 22:06:03 +0330

    backend - debbug di

--- backend/src/profile/profile.module.ts
+++ backend/src/profile/profile.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProfileResolver } from './profile.resolver';
 import { ProfileService } from './profile.service';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [ProfileResolver, ProfileService]
+  providers: [ProfileResolver, ProfileService, PrismaService],
 })
 export class ProfileModule {}
