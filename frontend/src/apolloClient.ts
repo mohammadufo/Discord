3adc170e35ae9db9df3b29cd35f158aec8a50230
Macbook <shadow.m.s.313@gmail.com>
2024-06-18 23:40:00 +0330

    add apollo upload link

--- frontend/src/apolloClient.ts
+++ frontend/src/apolloClient.ts
@@ -1,6 +1,27 @@
+import { setContext } from '@apollo/client/link/context'
+import { createUploadLink } from 'apollo-upload-client'
+
 const getToken = (name: string) => {
   const value = `; ${document.cookie}`
   console.log(document.cookie)
   const parts = value.split(`; ${name}=`)
   if (parts.length === 2) return parts.pop()?.split(';').shift()
 }
+
+// auth link
+const authLink = setContext(async (_, { headers }) => {
+  const token = getToken('__session')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
+
+const uploadLink = createUploadLink({
+  uri: 'http://localhost:3000/graphql',
+  headers: {
+    'apollo-require-preflight': 'true',
+  },
+})
