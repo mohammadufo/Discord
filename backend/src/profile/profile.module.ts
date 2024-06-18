48af7c18696ea1f126da78bc00900973697601b7
Macbook <shadow.m.s.313@gmail.com>
2024-06-18 23:08:06 +0330

    backend - apply auth guards

--- backend/src/profile/profile.module.ts
+++ backend/src/profile/profile.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ProfileResolver } from './profile.resolver';
 import { ProfileService } from './profile.service';
 import { PrismaService } from 'src/prisma.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  providers: [ProfileResolver, ProfileService, PrismaService],
+  providers: [ProfileResolver, ProfileService, PrismaService, JwtService],
 })
 export class ProfileModule {}
--- backend/src/profile/profile.resolver.ts
+++ backend/src/profile/profile.resolver.ts
@@ -2,16 +2,20 @@ import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { ProfileService } from './profile.service';
 import { Profile } from './profile.type';
 import { CreateProfileDto } from './dtos/create-profile.dto';
+import { UseGuards } from '@nestjs/common';
+import { GraphqlAuthGuard } from 'src/auth/auth.guard';
 
 @Resolver()
 export class ProfileResolver {
   constructor(private readonly profileService: ProfileService) {}
 
+  @UseGuards(GraphqlAuthGuard)
   @Mutation(() => Profile)
   async create(@Args('input') input: CreateProfileDto) {
     return this.profileService.createProfile(input);
   }
 
+  @UseGuards(GraphqlAuthGuard)
   @Query(() => Profile)
   async getProfileById(@Args('profiledId') profileId: number) {
     return this.profileService.getProfileById(profileId);
