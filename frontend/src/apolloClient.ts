cd07bbbee1289a130ca2e96b2a22e729146542b3
Macbook <shadow.m.s.313@gmail.com>
2024-06-19 22:04:36 +0330

    frontend - add error link

--- frontend/src/apolloClient.ts
+++ frontend/src/apolloClient.ts
@@ -1,5 +1,6 @@
 import { setContext } from '@apollo/client/link/context'
 import { createUploadLink } from 'apollo-upload-client'
+import { onError } from '@apollo/client/link/error'
 
 const getToken = (name: string) => {
   const value = `; ${document.cookie}`
@@ -24,4 +25,18 @@ const uploadLink = createUploadLink({
   headers: {
     'apollo-require-preflight': 'true',
   },
+})
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) => {
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    })
+
+    if (networkError) {
+      console.log(`[Network error]: ${networkError}`)
+    }
+  }
 })
