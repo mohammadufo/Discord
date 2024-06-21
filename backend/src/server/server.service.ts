4cb027a41a7004e0fd25849494f621899721e6e5
Macbook <shadow.m.s.313@gmail.com>
2024-06-21 10:14:26 +0330

    backend - implement create server on service

--- backend/src/server/server.service.ts
+++ backend/src/server/server.service.ts
@@ -1,7 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import { CreateServerDto } from './dtos/create-server.dto';
+import { v4 as uuidv4 } from 'uuid';
+import { MemberRole } from 'src/member/member.types';
 
 @Injectable()
 export class ServerService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async createServer(input: CreateServerDto, imageUrl: string) {
+    const profile = await this.prismaService.profile.findUnique({
+      where: {
+        id: input.profileId,
+      },
+    });
+
+    if (!profile) {
+      throw new BadRequestException('Profile not found!');
+    }
+
+    return this.prismaService.server.create({
+      data: {
+        ...input,
+        imageUrl,
+        inviteCode: uuidv4(),
+
+        channels: {
+          create: [
+            {
+              name: 'general',
+              profileId: profile.id,
+            },
+          ],
+        },
+        members: {
+          create: [
+            {
+              profileId: profile.id,
+              role: MemberRole.ADMIN,
+            },
+          ],
+        },
+      },
+      include: {
+        members: true,
+      },
+    });
+  }
 }
