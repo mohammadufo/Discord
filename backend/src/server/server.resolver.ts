3bb5e2da7e2d958bb72dcb9911bf7129cbd8620d
Macbook <shadow.m.s.313@gmail.com>
2024-06-21 11:53:40 +0330

    backend - implement storeimage functionality

--- backend/src/server/server.resolver.ts
+++ backend/src/server/server.resolver.ts
@@ -1,10 +1,15 @@
-import { Args, Context, Query, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { Server } from './types';
 import { UseGuards } from '@nestjs/common';
 import { GraphqlAuthGuard } from 'src/auth/auth.guard';
 import { ApolloError } from 'apollo-server-express';
 import { ServerService } from './server.service';
 import { IUpdatedRequest } from '../types/index';
+import * as GraphQLUpload from 'graphql-upload/GraphQLUpload.js';
+import { v4 as uuidv4 } from 'uuid';
+import { join } from 'path';
+import { createWriteStream, existsSync, mkdirSync } from 'fs';
+import { CreateServerDto } from './dtos/create-server.dto';
 
 @UseGuards(GraphqlAuthGuard)
 @Resolver()
@@ -24,5 +29,20 @@ export class ServerResolver {
     return await this.serverService.getServersByProfileEmailOfMember(
       ctx.req?.profile.email,
     );
+  }
+
+  async storeImageAndGetUrl(file: GraphQLUpload) {
+    const { createReadStream, filename } = await file;
+    const uniqueFilename = `${uuidv4()}_${filename}`;
+    const imagePath = join(process.cwd(), 'public', 'images', uniqueFilename);
+    const imageUrl = `${process.env.APP_URL}/images/${uniqueFilename}`;
+
+    if (!existsSync(join(process.cwd(), 'public', 'images'))) {
+      mkdirSync(join(process.cwd(), 'public', 'images'), { recursive: true });
+    }
+
+    const readStream = createReadStream();
+    readStream.pipe(createWriteStream(imagePath));
+    return imageUrl;
   }
 }
