778159a06b8ac9043f6928d1228bf017c08ba2f4
Macbook <shadow.m.s.313@gmail.com>
2024-06-22 19:27:32 +0330

    frontend - implment server sidebar

--- frontend/src/components/navigation/ServerSidebar.tsx
+++ frontend/src/components/navigation/ServerSidebar.tsx
@@ -1,10 +1,29 @@
+import { ScrollArea } from '@mantine/core'
 import ServerHeader from './ServerHeader'
+import classes from './ServerSidebar.module.css'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useServer } from '../../hooks/graphql/server/useServer'
 
 function ServerSidebar() {
+  const navigate = useNavigate()
+
+  const { serverId, memberId, channelId } = useParams()
+  const { textChannels, audioChannels, videoChannels, server, role, members } =
+    useServer()
+
+  useEffect(() => {
+    if (!channelId && !memberId && textChannels.length) {
+      navigate(`/servers/${serverId}/channels/TEXT/${textChannels[0]?.id}`)
+    }
+  })
+
   return (
-    <div>
+    <nav className={classes.nav}>
       <ServerHeader />
-    </div>
+      {/* ServerSearch */}
+      <ScrollArea></ScrollArea>
+    </nav>
   )
 }
 
