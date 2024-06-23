04e82bb6726a51b1c09bc822fd4bd0938151ecbd
Macbook <shadow.m.s.313@gmail.com>
2024-06-23 20:08:27 +0330

    backend - add updateServerWithNewInviteCode in server service

--- backend/src/server/server.service.ts
+++ backend/src/server/server.service.ts
@@ -1,9 +1,17 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 import { CreateServerDto } from './dtos/create-server.dto';
 import { v4 as uuidv4 } from 'uuid';
 import { MemberRole } from 'src/member/member.types';
 import { ApolloError } from 'apollo-server-express';
+import { UpdateServerDto } from './dtos/update-server.dto';
+import { CreateChannelOnServerDto } from './dtos/create-channel.dto';
+import { ChannelType } from './types';
 @Injectable()
 export class ServerService {
   constructor(private readonly prisma: PrismaService) {}
@@ -88,6 +96,84 @@ export class ServerService {
             profile: {
               email,
             },
+          },
+        },
+      },
+    });
+  }
+
+  async updateServerWithNewInviteCode(serverId: number) {
+    const server = await this.prisma.server.findUnique({
+      where: {
+        id: serverId,
+      },
+    });
+
+    if (!server) {
+      throw new NotFoundException('Server Not found!');
+    }
+
+    return this.prisma.server.update({
+      where: {
+        id: serverId,
+      },
+      data: {
+        inviteCode: uuidv4(),
+      },
+    });
+  }
+
+  async updateServer(input: UpdateServerDto, imageUrl: string) {
+    const server = await this.prisma.server.findUnique({
+      where: {
+        id: input.serverId,
+      },
+    });
+
+    if (!server) {
+      throw new NotFoundException('Server not found!');
+    }
+
+    return this.prisma.server.update({
+      where: {
+        id: input.serverId,
+      },
+      data: {
+        name: input.name,
+        imageUrl,
+      },
+    });
+  }
+
+  async createChannel(input: CreateChannelOnServerDto, email: string) {
+    const profile = await this.prisma.profile.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!profile) {
+      throw new UnauthorizedException();
+    }
+
+    return this.prisma.server.update({
+      where: {
+        id: input.serverId,
+        members: {
+          some: {
+            profileId: profile.id,
+            role: {
+              in: [MemberRole.ADMIN, MemberRole.MODERATOR],
+            },
+          },
+        },
+      },
+      data: {
+        channels: {
+          create: {
+            name: input.name,
+            profileId: profile.id,
+            type: ChannelType[input.type],
           },
         },
       },
