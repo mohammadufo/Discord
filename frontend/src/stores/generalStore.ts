3589c7c85f4faffe7d7b5a869c593b02de4cfd0b
Macbook <shadow.m.s.313@gmail.com>
2024-06-25 13:14:59 +0330

    frontend - update general store

--- frontend/src/stores/generalStore.ts
+++ frontend/src/stores/generalStore.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
+import { ChannelType } from '../gql/graphql'
 
 export type Modal =
   | 'CreateServer'
@@ -15,19 +16,26 @@ export type Modal =
   | 'LeaveServer'
 
 interface GeneralStore {
-  activeModal: Modal | null
-  setActiveModal: (modal: Modal | null) => void
   drawerOpen: boolean
+  activeModal: Modal | null
+  chanelTypeForCreateChannelModal: ChannelType | undefined
+
   toggleDrawer: () => void
+  setActiveModal: (modal: Modal | null) => void
+  setChannelTypeForCreateChannelModal: (type: ChannelType | undefined) => void
 }
 
 export const useGeneralStore = create<GeneralStore>()(
   persist(
     (set) => ({
       activeModal: null,
+      drawerOpen: true,
+      chanelTypeForCreateChannelModal: ChannelType.Text,
+
       setActiveModal: (modal: Modal | null) => set({ activeModal: modal }),
-      drawerOpen: false,
       toggleDrawer: () => set((state) => ({ drawerOpen: !state.drawerOpen })),
+      setChannelTypeForCreateChannelModal: (channeltype) =>
+        set(() => ({ chanelTypeForCreateChannelModal: channeltype })),
     }),
     {
       name: 'general-store',
